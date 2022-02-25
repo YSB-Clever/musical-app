45e610e4b8a71ae0edd7b9f109e3305ac70d6674
YSB-Clever <99031182+YSB-Clever@users.noreply.github.com>
2022-02-25 22:49:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     create_app
     app.run(debug=True)
 
-        template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+        template_dir = os.path.join(os.path.dirname(__login.html__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),autoescape = True)
