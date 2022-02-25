d8067fc8672495d5ac3b920e526a876e767be2ec
YSB-Clever <99031182+YSB-Clever@users.noreply.github.com>
2022-02-25 22:44:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     create_app
     app.run(debug=True)
 
-env = Environment(loader=FileSystemLoader(searchpath='C:\website\templates')
+env = Environment(loader=FileSystemLoader(searchpath='C:\website\Templates')
 template = env.get_template('login.html')
